d3ed835bb9971caa319da66a0a88c85059cf22c9
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2024-02-28 13:38:37 +0100

    Fix: Set up the difference components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Different from "./components/Different";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import "./index.css"
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero/>
+      <Different/>
     </>
   );
 }
---
+++ src/components/Different.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Lightening from "../assets/icon-snappy-process.svg";
+import Prices from "../assets/icon-affordable-prices.svg";
+
+const Different = () => {
+  return (
+    <section className="different w-100 ">
+      <div className="w-50 ">
+        <div className="line"></div>
+        <h1 className="heading_fix mb-3 text-capitalize">we're different</h1>
+      </div>
+      <div className="w-100 d-flex align-items-center justify-content-center mt-5 row">
+        <div className="details_card col-lg-4 col-12">
+          <img src={Lightening} alt="a" />
+          <span className="fs-3">Snappy Process</span>
+          <p className="mt-3">
+            Our application process can be completed in minutes, not hours.
+            Don't get stuck filling in tedious forms.
+          </p>
+        </div>
+        <div className="details_card col-lg-4 col-12">
+          <img src={Prices} alt="b" />
+          <span className="fs-3">Affordable Prices</span>
+          <p className="mt-3">
+            We don't want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </p>
+        </div>
+        <div className="details_card col-lg-4 col-12">
+          <img src={Lightening} alt="a" />
+          <span className="fs-3">People First</span>
+          <p className="mt-3">
+            Our plans aren't full of conditions and clauses to prevent payouts.
+            We make sure you're covered when you need it.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Different;
